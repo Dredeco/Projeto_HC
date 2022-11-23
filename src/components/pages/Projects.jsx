a74ecb9d66a8d9e6238a74791f618c9e44475486
André Fernandes <andrefersouza@gmail.com>
2022-11-23 11:44:05 -0300

    update

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
+
+import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
+
+import styles from './Projects.module.sass'
 
 export default function Projects() {
+
+  const location = useLocation()
+  let message = ''
+  if (location.state) {
+    message = location.state.message
+  }
+
   return (
-    <div>Projects</div>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto" />
+      </div>
+      {message && <Message msg={message} type="success" />}
+      <Container customClass="Start">
+        <p>Projetos...</p>
+      </Container>
+    </div>
   )
 }
